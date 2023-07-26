c2428bbf462abb610b4b4124a52088af5624357c
Frankenstei <enwereloveday@yahoo.com>
2023-07-26 06:34:43 -0700

    all

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSign(uploadPhoto, createUser) {
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((message) => {
+      console.log(`${message[0].body} ${message[1].firstName} ${message[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
